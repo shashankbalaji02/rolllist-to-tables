3459cd79c116d4c2a6c134ea0e296ec91ebbfa51
Shashank Balaji <shashankbalaji02@gmail.com>
2021-12-02 18:40:06 +0530

    Refactored 2021

--- Roll-List-2021.py
+++ Roll-List-2021.py
@@ -63,81 +63,75 @@ import tabula
 import pandas
 import re
 
-def numeric_part_of_string(s):
-    return int(re.compile(r"\d+").findall(s)[0])
-
 mapping = {
-    "001": ["Aerospace Engineering", "AE"],
-    "002": ["Chemical Engineering", "CL"],
-    "004": ["Civil Engineering", "CE"],
-    "005": ["Computer Science and Engineering", "CS"],
-    "007": ["Electrical Engineering", "EE"],
-    "010": ["Mechanical Engineering", "ME"],
-    "011": ["Metallurgical Engineering and Materials Science", "MM"],
-    "026": ["Engineering Physics", "EP"],
-    "D07": ["Electrical Engineering Dual", "EED"],
-    "D10": ["Mechanical Engineering Dual", "MED"],
-    "D11": ["Metallurgical Engineering and Materials Science Dual", "MMD"],
-    "D17": ["Energy Science and Engineering", "EN"],
-    "D18": ["Environmental Science & Engineering Dual", "ES"],
-    "B03": ["Chemistry", "CH"],
-    "B08": ["Humanities and Social Sciences", "EO"],
-    "B09": ["Mathematics", "MA"]
+    "001": ("Aerospace Engineering", "AE"),
+    "002": ("Chemical Engineering", "CL"),
+    "004": ("Civil Engineering", "CE"),
+    "005": ("Computer Science and Engineering", "CS"),
+    "007": ("Electrical Engineering", "EE"),
+    "010": ("Mechanical Engineering", "ME"),
+    "011": ("Metallurgical Engineering and Materials Science", "MM"),
+    "026": ("Engineering Physics", "EP"),
+    "D07": ("Electrical Engineering Dual", "EED"),
+    "D10": ("Mechanical Engineering Dual", "MED"),
+    "D11": ("Metallurgical Engineering and Materials Science Dual", "MMD"),
+    "D17": ("Energy Science and Engineering", "EN"),
+    "D18": ("Environmental Science & Engineering Dual", "ES"),
+    "B03": ("Chemistry", "CH"),
+    "B08": ("Humanities and Social Sciences", "EO"),
+    "B09": ("Mathematics", "MA")
 }
+
+def remove_first_column(df):
+    df.drop(df.columns[0], axis = 1, inplace = True)
+
+def add_column_labels(df):
+    # Duplicate the very first entry in the dataframe
+    df.loc[-1] = list(df)
+    df.index = df.index + 1
+    df.sort_index(inplace = True)
+    # Set the correct column lables
+    df.set_axis(["Roll Number", "Name", "Division", "Tutorial", "Lab"], axis = 1, inplace = True)
+
+def remove_newline_from_names(df):
+    df["Name"] = df["Name"].apply(lambda s: s.replace("\r", " "))
+
+def join_split_name(df1, df2):
+    if df2["Roll Number"][0].startswith("Unnamed"):
+        last_name = df2["Name"][0]
+        df2.drop(0, inplace = True)
+        df2.reset_index(drop = True, inplace = True)
+        column = df1["Name"]
+        first_name = column[len(column) - 1]
+        df1["Name"][len(column) - 1] = first_name + " " + last_name
+
+def numerify_division_tutorial_lab(df):
+    for column in ["Division", "Tutorial", "Lab"]:
+        df[column] = df[column].apply(lambda s: int(re.compile(r"\d+").findall(s)[0]))
+
+def stringify_roll_number(df):
+    df["Roll Number"] = df["Roll Number"].apply(lambda x: str(x))
+
+def add_branch_and_branch_codes(df):
+    branch_number = df["Roll Number"][0][2:5]
+    branch_info = mapping[branch_number]
+    df["Branch"] = branch_info[0]
+    df["Branch Code"] = branch_info[1]
 
 # Get a list of dataframes; one dataframe for each table in each page
 dfs = tabula.read_pdf(input_str, pages = "all")
 
 for df in dfs:
-    # Drop the first column in each dataframe
-    df.drop(df.columns[0], axis = 1, inplace = True)
+    remove_first_column(df)
+    add_column_labels(df)
+    remove_newline_from_names(df)
 
-    # Duplicate the very first entry in the dataframe
-    df.loc[-1] = list(df)
-    df.index = df.index + 1
-    df.sort_index(inplace = True)
-
-    # Set the correct column lables
-    df.set_axis(["Roll Number", "Name", "Division", "Tutorial", "Lab"], axis = 1, inplace = True)
-
-# Because of some long names, the tails of those names get split into two tables
-# This function is to remove the first row of the dataframe at index and append its name to the dataframe at index - 1
-def join_split_name(dfs, index):
-    current_df = dfs[index]
-    ending = current_df["Name"][0]
-    current_df.drop(0, inplace = True)
-    current_df.reset_index(inplace = True)
-    current_df.drop(current_df.columns[0], axis = 1, inplace = True)
-    column = dfs[index - 1]["Name"]
-    partial_name = column[len(column) - 1]
-    dfs[index - 1]["Name"][len(column) - 1] = partial_name + " " + ending
-
-# These are the places of anomaly
-for i in [12, 13, 29, 37, 38]:
-    join_split_name(dfs, i)
+for (df1, df2) in zip(dfs, dfs[1:]):
+    join_split_name(df1, df2)
 
 for df in dfs:
-    # Get rid of the Ds, Ts, and the Ps
-    for column in ["Division", "Tutorial", "Lab"]:
-        df[column] = df[column].apply(numeric_part_of_string)
-    
-    # Convert all the roll numbers to strings if at all some of them are numbers
-    df["Roll Number"] = df["Roll Number"].apply(lambda x: str(x))
+    numerify_division_tutorial_lab(df)
+    stringify_roll_number(df)
+    add_branch_and_branch_codes(df)
 
-    # Remove '\r' in the names
-    df["Name"] = df["Name"].apply(lambda s: s.replace("\r", " "))
-
-    # Add the course and course code
-    branch_number = df["Roll Number"][0][2:5]
-    try:
-        branch_info = mapping[branch_number]
-    except KeyError:
-        print(f"What does course number '{branch_number}' at index {i} mean?")
-        raise
-    else:
-        df["Branch"] = branch_info[0]
-        df["Branch Code"] = branch_info[1]
-
-result = pandas.concat(dfs).reset_index()
-result.drop(result.columns[0], axis = 1, inplace = True)
-result.to_excel(output, index = False)
+pandas.concat(dfs).to_excel(output, index = False)
